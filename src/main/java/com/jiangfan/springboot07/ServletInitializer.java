00499948fa32acaa47ca925fc803c3c146cd9ff7
jiangfan <jiangfan@greatwall.com.cn>
2020-08-27 13:36:25 +0800

    20200827

---
+++ src/main/java/com/jiangfan/springboot07/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jiangfan.springboot07;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Springboot07Application.class);
+	}
+
+}
